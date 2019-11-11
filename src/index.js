fc03b1eca29fee8d3568513d4383b7bcc409a2f1
Rodrigo Maximo <rodrigomaximocano@gmail.com>
2019-11-11 18:40:08 -0300

    Full test commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { Store } from './store'
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
+
+serviceWorker.unregister();
